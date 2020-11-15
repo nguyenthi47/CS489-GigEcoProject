b5d2c52146c09d61b9c26875a5cb1e55f09a2aeb
gcamilo <gcamilo7@gmail.com>
2020-11-16 08:31:42 +0900

    fix conflict

--- src/Study.js
+++ src/Study.js
@@ -85,93 +85,6 @@ class Study extends React.Component {
       alert("Please answer all questions.");
     }
 
-<<<<<<< HEAD
-    updateProgress() {
-        var count = 0;
-        var bar = document.getElementById('ProgressBar');
-        count = count + 100 / 16;
-        bar.style.width = count + '%';
-    }
-
-    render() {
-        let content;
-        content = 
-            <div>
-                <div className="Subtitle" style={{marginBottom: "10px"}}>
-                    Please read the following job listings for professional drivers below.
-                </div>
-                <div className="BigText" style={{marginTop: "10px"}}>
-                    We would like to know your opinion about both of them and which one you would prefer to take if you were given the chance to choose.
-                </div>
-                <hr/>
-                <div>
-                    <div>
-                        <Table pair={this.props.pairs[this.state.idx]}/>
-                    </div>
-                </div>
-                <hr/>
-                <div>
-                    <div>
-                        <div className="QuestionMargin Question">
-                            To what extent would you be willing to accept <span className="underline">job listing 1</span>?
-                        </div>
-                        <LikertScale
-                            n={5}
-                            currId={this.state.idx}
-                            saveResponse={this.props.saveResponse}
-                            question={"j" + this.state.idx + "-1"}
-                            multipleText={true}
-                            answers={["Not willing at all", "Not really willing", "Undecided", "Somewhat willing", "Willing"]}
-                        />
-                    </div>
-
-                    <div>
-                        <div className="QuestionMargin Question">
-                            To what extent would you be willing to accept <span className="underline">job listing 2</span>?
-                        </div>
-                        <LikertScale
-                            n={5}
-                            currId={this.state.idx}
-                            saveResponse={this.props.saveResponse}
-                            question={"j" + this.state.idx + "-2"}
-                            multipleText={true}
-                            answers={["Not willing at all", "Not really willing", "Undecided", "Somewhat willing", "Willing"]}
-                        />
-                    </div>
-
-
-                    <div>
-                        <div className="QuestionMargin Question">
-                            Which one of the two job listings above do you prefer?
-                        </div>
-                        <LikertScale
-                            n={2}
-                            currId={this.state.idx}
-                            saveResponse={this.props.saveResponse}
-                            question={"choice" + this.state.idx}
-                            multipleText={true}
-                            answers={["Job Listing 1", "Job Listing 2"]}
-                        />
-                    </div>
-                    
-                </div>
-                <hr style={{marginTop: "30px"}}/>
-                <div style={{textAlign: "right"}}>
-                <Button variant="secondary" onClick={ this.nextScenario}>Next</Button>
-                    <div style={{display: (this.state.attCheck) ? "none" : "block", textAlign: "left"}}>{this.state.idx + 1}/16</div>
-            </div>
-        <div id="ProgressCase">
-            <div id="ProgressBar"></div>
-        </div>
-            </div>
-        return(
-            <div>
-                {content}
-            </div>
-        );
-    }
-
-=======
   }
   render() {
     const { classes } = this.props;
@@ -206,7 +119,6 @@ class Study extends React.Component {
       </React.Fragment>
     );
   }
->>>>>>> 720366579752938505874355e8876f1340d9af78
 }
 
 export default withStyles(styles) (Study);
