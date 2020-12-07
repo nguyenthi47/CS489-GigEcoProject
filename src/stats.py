b53ccd9523d8f343815dbd569e46b5ae4db8c3b8
Quan Nguyen <quannguyenvan1701@gmail.com>
2020-12-07 13:19:20 +0700

    Add mean evaluation

--- src/stats.py
+++ src/stats.py
@@ -6,10 +6,11 @@ df = pd.read_csv(file_name)
 
 attrs = ['Day off', 'Direct Supervisor', 'Evaluation', 'Salary', 'Working Options (companies in same sector)', 'Working hours (length)', 'Working hours (time period)']
 
-print('Data summary')
+print('Data summary\n')
 for attr in attrs:
-    print('\n' + attr)
+    print('\n' + attr + '\n')
+    print('Percentage')
     print(df[df.choice == True][attr].value_counts(normalize=True))
-
-print('\nMean evaluation')
-print(df['eval'].mean())
+    print('\nMean evaluation')
+    for value in df[attr].unique():
+        print(f"{value}\t{df[df[attr] == value]['eval'].mean()}")
