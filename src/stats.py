d1f80f4eb18a5d8186479b9ad6d23a594dcefbe2
Quan Nguyen <quannguyenvan1701@gmail.com>
2020-12-07 11:08:20 +0700

    Add data summary

---
+++ src/stats.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+file_name = '../survey_data/df.csv'
+
+df = pd.read_csv(file_name)
+
+attrs = ['Day off', 'Direct Supervisor', 'Evaluation', 'Salary', 'Working Options (companies in same sector)', 'Working hours (length)', 'Working hours (time period)']
+
+print('Data summary')
+for attr in attrs:
+    print('\n' + attr)
+    print(df[attr].value_counts(normalize=True))
